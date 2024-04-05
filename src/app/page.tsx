b421930ffaeb575f717a447442048111d026c53e
robskrob <jewell.robertp@gmail.com>
2024-04-05 17:58:10 -0400

    deploy script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p>Testing <span className="blue-color">this</span> bucket </p>
+        <Image src="/images/ChewbaccaCSWE.jpeg" alt="chewy" width={300} height={300}/>
       </div>
     </main>
   );
