2c360bcb997a7957448cd7fe0d7ed81d8078d8d2
robskrob <jewell.robertp@gmail.com>
2024-04-06 14:25:25 -0400

    deploy script to create image meta data
    so that page knows the image path

---
+++ scripts/create-images-meta.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+
+const client = new S3Client({});
+
+(async () => {
+  const command = new ListObjectsV2Command({
+    Bucket: "web-origin-dark-room-app",
+    Prefix: "images"
+  });
+
+  try {
+    const response = await client.send(command);
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    console.log("truncated: ", response.IsTruncated)
+    console.log("file count: ", response.KeyCount)
+    console.log(response.Contents)
+
+    const contents = response.Contents;
+    const list = []
+    for (const index in contents) {
+      const asset = contents[index];
+      list.push({alt: `image ${index + 1}`, path: asset.Key})
+    }
+
+    await fs.writeFileSync('./out/meta-images.json', JSON.stringify(list));
+
+  } catch (err) {
+    console.error(err);
+  }
+})();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('http://web-origin-dark-room-app.s3-website-us-east-1.amazonaws.com/meta-images.json')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log("data: ", data)
+  const imageList = [];
+  for (const index in data) {
+    const asset = data[index];
+    imageList.push(<li><Image src={"http://web-origin-dark-room-app.s3-website-us-east-1.amazonaws.com/" + asset.path} alt={asset.alt} width={300} height={300}/></li>)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p>Testing <span className="blue-color">this</span> bucket </p>
-        <Image src="/images/ChewbaccaCSWE.jpeg" alt="chewy" width={300} height={300}/>
+        <ul>{imageList}</ul>
       </div>
     </main>
   );
