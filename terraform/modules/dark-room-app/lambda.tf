3127dd4f130352437e7e1a8a0a2fea5bb653c513
=robskrob <agentcooper_aces@proton.me>
2025-03-09 17:24:06 -0400

    lambda can read s3 bucket

--- terraform/modules/dark-room-app/lambda.tf
+++ terraform/modules/dark-room-app/lambda.tf
@@ -47,24 +47,32 @@ resource "aws_iam_role_policy" "lambda_role_sqs_policy" {
 EOF
 }
 
-resource "aws_iam_role_policy" "lambda_role_s3_policy" {
-    name = "AllowS3Permissions"
-    role = "${aws_iam_role.lambda_role.id}"
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "${resource.aws_s3_bucket.image_bucket.arn}"
-    }
-  ]
+resource "aws_iam_role_policy_attachment" "lambda_s3_read_policy" {
+
+  role = aws_iam_role.lambda_role.id
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+
 }
-EOF
-}
+
+# resource "aws_iam_role_policy" "lambda_role_s3_policy" {
+#     name = "AllowS3Permissions"
+#     role = "${aws_iam_role.lambda_role.id}"
+#     policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "s3:GetObject"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "${resource.aws_s3_bucket.image_bucket.arn}"
+#     }
+#   ]
+# }
+# EOF
+# }
 
 resource "aws_iam_role_policy" "lambda_role_logs_policy" {
     name = "LambdaRolePolicy"
