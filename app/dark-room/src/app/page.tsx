ffa4ae05b8fa45bf7ec3a3deaf5d4a04bc0b519e
robskrob <agentcooper_aces@proton.me>
2025-04-25 13:59:18 -0400

    WIP

--- app/dark-room/src/app/page.tsx
+++ app/dark-room/src/app/page.tsx
@@ -2,15 +2,14 @@
 
 import ImageGallery from "./image-gallery";
 
-
 async function getData() {
-  const res = await fetch('https://www.darkroom.cfd/meta-images.json')
+  const res = await fetch("https://www.darkroom.cfd/meta-images.json");
 
   if (!res.ok) {
-    throw new Error('index page getData, Failed to fetch data')
+    throw new Error("index page getData, Failed to fetch data");
   }
 
-  return res.json()
+  return res.json();
 }
 
 export default async function Home() {
@@ -19,7 +18,7 @@ export default async function Home() {
   return (
     <main className="min-h-screen items-center justify-center flex flex-col">
       <div className="main-padding main-max-width  z-10 w-full justify-center font-mono text-sm lg:flex">
-        <ImageGallery imageData={data}/>
+        <ImageGallery imageData={data} />
       </div>
     </main>
   );
